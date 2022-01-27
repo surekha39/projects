034d302f3ea8db3c7221863df044aba660cf2386
surekha39 <97872102+surekha39@users.noreply.github.com>
2022-01-27 12:52:14 +0000

    Add files via upload

---
+++ src/com/queue/QueueExample.java
@@ -0,0 +1,22 @@
+package com.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample 
+{
+public static void main(String[] args) 
+{
+        		Queue<String> locationsQueue = new LinkedList<>();
+locationsQueue.add("Vijayawada");
+        		locationsQueue.add("Pune");
+        		locationsQueue.add("Delhi");
+        		locationsQueue.add("Gujarat");
+        		locationsQueue.add("Chennai");
+System.out.println("Queue is : " + locationsQueue);
+        		System.out.println("Head of Queue : " + locationsQueue.peek());
+        		locationsQueue.remove();
+        		System.out.println("After removing Head of Queue : " + locationsQueue);
+        		System.out.println("Size of Queue : " + locationsQueue.size());
+    	}
+}
